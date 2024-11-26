7f03ca63676760a950fa43c7f10bdf76d0163856
eliasasa <eliasnetoneto1@hotmail.com>
2024-11-26 19:01:44 -0400

    telas 50%
    adicionando telas cadastro de usuário e login (não funcionais), e conexão entre as páginas

--- home_adm.py
+++ home_adm.py
@@ -3,34 +3,70 @@ from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 from controllers.biblioteca import Biblioteca as B
 
-ui_file_home = 'views/home_adm.ui'
-ui_file_cad = 'views/cad_livro.ui'
+ui_file_home = 'views/homeAdm.ui'
+ui_file_cadLivro = 'views/cadLivro.ui'
+ui_file_cadUser = 'views/cadUser.ui'
+ui_file_login = 'views/logUser.ui'
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi(ui_file_home, self)
-        self.cad_livro: QPushButton
-        self.cad_livro.clicked.connect(self.tela_cad_livro)  
+        uic.loadUi(ui_file_login, self)
+        self.logBut.clicked.connect(self.logar)
+        self.cadBut.clicked.connect(self.tela_cad_user)
+        self.login = False
+
+    def logar(self):
+        usuario = self.logUser.text()
+        senha = self.logSen.text()
+
+        # Fazer tratamento, criar table de adm e emprestimo
+
+        print(usuario, senha)
+
+        self.login = True
+        self.voltar_home()
+    
+    def tela_cad_user (self):
+        # Carrrega tela de cadastro de usuário
+        uic.loadUi(ui_file_cadUser, self)
+        self.cadUserBut.clicked.connect(self.click_cad_user)
+        self.cadUser_home.clicked.connect(self.voltar_home)
 
     def tela_cad_livro(self):
         # Carregar a tela de cadastro de livro
-        uic.loadUi(ui_file_cad, self)
-        self.butao.clicked.connect(self.click_cad) 
+        uic.loadUi(ui_file_cadLivro, self)
+        self.butao.clicked.connect(self.click_cad_livro) 
         self.cad_home.clicked.connect(self.voltar_home)  
 
+    # Voltar
     def voltar_home(self):
-        uic.loadUi(ui_file_home, self)
-        self.cad_livro.clicked.connect(self.tela_cad_livro)
+        if self.login:
+            uic.loadUi(ui_file_home, self)
+            self.cad_livro.clicked.connect(self.tela_cad_livro)
+            self.cad_user.clicked.connect(self.tela_cad_user)
+        else:
+            uic.loadUi(ui_file_login, self)
+            self.logBut.clicked.connect(self.logar)
+            self.cadBut.clicked.connect(self.tela_cad_user)
 
-    def click_cad(self):
+    # Funções (services)
+    def click_cad_livro(self):
         titulo = self.nTitle.text()
         autor = self.nAutor.text()
         genero = self.nGen.text()
         codigo = self.nCod.text()
         B.add_livro(titulo, autor, genero, 'Disponível', codigo)
-        
+    
+    def click_cad_user(self):
+        nome = self.nomeCad.text()
+        cpf = self.cpfCad.text()
+        telefone = self.telCad.text()
+        senha = self.senCad.text()
+        email = self.emailCad.text()
 
+        print(nome, cpf, telefone, senha, email)
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
